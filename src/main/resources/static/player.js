2047bf1957dc52843ee884243c87bf7e0c763576
MuhammadShayekurRahman <shayek.rahman@gmail.com>
2022-07-15 16:39:32 +0100

    Basic front-end creates, reads and deletes player

--- src/main/resources/static/player.js
+++ src/main/resources/static/player.js
@@ -19,7 +19,43 @@ document.querySelector("body > form").addEventListener("submit", function(event)
             console.log("Res:", response)
             this.reset();
             this.firstName.focus();
-            //renderCars();
+            renderPlayers();
         })
         .catch(error => console.error(error))
 })
+
+function renderPlayers(){
+    axios.get("http://localhost:8080/getPlayers")
+        .then(response => {
+            console.log(response);
+
+            output.innerHTML="";
+
+            for(let player of response.data){
+                const playerEntry = document.createElement("p");
+                playerEntry.innerText = "Player = Firstname: [" + player.firstName + "] Surname: [" + player.surname + "] Position: [" + player.position + "]  Shirt Number: [" + player.shirtNumber + "] Team: [" +player.teamName + "]";
+                output.appendChild(playerEntry);
+
+                const playerDelete = document.createElement("button");
+                playerDelete.innerText = "Delete";
+                output.appendChild(playerDelete);
+                playerDelete.addEventListener("click", () =>{
+                    deletePlayer(player.id);
+                })
+
+            }
+
+        })
+        .catch(error => console.error(error))
+}
+
+const deletePlayer = (id) =>{
+    axios.delete("http://localhost:8080/removePlayer/" + id)
+        .then(response => {
+            console.log("delete successful")
+            renderPlayers();
+        })
+        .catch(error => console.error(error))
+}
+
+renderPlayers();
